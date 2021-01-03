3ca0cff43b3d02e490112353a00454bc6e3f3a94
JoyDas <joydascsepuc@gmail.com>
2021-01-03 11:53:54 +0600

    Base Design -- With PagesController

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+
+	<h1>Index Page</h1>
+	<p>This is Index Page. Pages Controller controls it by the web-route.</p>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', 'App\Http\Controllers\PagesController@index')->name('homePage');
+
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
